b24fa92464d96f09e5f74d3f2a9307fb8f37c1bb
atordvairn <atordvairn@gmail.com>
2023-03-22 14:30:51 +0530

    gui

--- index.js
+++ index.js
@@ -1,11 +1,5 @@
-async function sleep(t) {
-  return new Promise((r) => {
-    setTimeout(() => {
-      r();
-    }, t);
-  });
-}
-
+var term = require("terminal-kit").terminal;
+const pack = require("./package.json");
 const cocoSsd = require("@tensorflow-models/coco-ssd");
 const tf = require("@tensorflow/tfjs-node");
 const fs = require("fs").promises;
@@ -13,20 +7,16 @@ const file = require("fs");
 
 const http = require("https");
 let counter = 0;
-let limit = 80000;
+let limit;
 let globalmodel;
 
-file.readdir("./face", (e, f) => {
-  if (e) {
-    console.log("run setup");
-  }
-  file.readdir("./notface", (e, ff) => {
-    if (e) {
-      console.log("run setup");
-    }
-    counter = f.length + ff.length;
+async function sleep(t) {
+  return new Promise((r) => {
+    setTimeout(() => {
+      r();
+    }, t);
   });
-});
+}
 
 async function getfile(counter) {
   return new Promise(async (r) => {
@@ -46,14 +36,11 @@ async function getfile(counter) {
           // after download completed close filestream
           filee.on("finish", async () => {
             filee.close();
-            console.log("Download Completed: " + counter);
-            console.log("pridicting");
             await fs.readFile(`./github/${counter}.jpg`).then(async (d) => {
               const imgTensor = await tf.node.decodeImage(new Uint8Array(d), 3);
               await globalmodel.detect(imgTensor).then(async (a) => {
                 if (a[0]) {
                   if (a[0].class == "person") {
-                    console.log(`${counter} is person`);
                     fs.rename(
                       `./github/${counter}.jpg`,
                       `./face/${counter}.jpg`
@@ -63,18 +50,22 @@ async function getfile(counter) {
                       `./github/${counter}.jpg`,
                       `./notface/${counter}.jpg`
                     );
-                    console.log(`${counter} is ${a[0].class}`);
                   }
                 } else {
                   fs.rename(
                     `./github/${counter}.jpg`,
                     `./notface/${counter}.jpg`
                   );
-                  console.log(`${counter} is nothing`);
                 }
               });
             });
             counter++;
+            progressBar.update(counter / limit);
+            if (counter / limit >= 1) {
+              intro(true);
+              term.green("ALL FILES DOWNLOADED AND PARSED! CHECK FOLDERS!\n");
+              process.exit(0);
+            }
             if (counter >= limit) {
               process.exit(0);
             }
@@ -86,10 +77,11 @@ async function getfile(counter) {
           console.log(e);
         });
     } else {
-      console.log(counter, "exists");
       r();
       counter++;
       if (counter >= limit) {
+        intro(true);
+        term.yellow("ALREADY DOWNLOADED THESE MUCH FILES\n");
         process.exit(0);
       }
       getfile(counter);
@@ -97,8 +89,70 @@ async function getfile(counter) {
   });
 }
 
-cocoSsd.load().then((v) => {
-  console.log("model loaded");
-  globalmodel = v;
-  getfile(counter);
+cocoSsd.load().then(async (v) => {
+  await term("enter an image processing limit (1000): ");
+  term.inputField({}, (e, input) => {
+    term.green("\nlimit set to %s\n", input);
+    console.clear();
+    limit = input || 1000;
+    globalmodel = v;
+    intro();
+    getfile(counter);
+  });
+});
+
+async function intro(without) {
+  if (without) {
+    console.clear();
+  }
+  console.log("╔═╗┬┌┬┐┬ ┬┬ ┬┌┐   ╔═╗┌─┐┬─┐┌─┐┌─┐┌─┐┬─┐");
+  console.log("║ ╦│ │ ├─┤│ │├┴┐  ╚═╗│  ├┬┘├─┤├─┘├┤ ├┬┘");
+  console.log("╚═╝┴ ┴ ┴ ┴└─┘└─┘  ╚═╝└─┘┴└─┴ ┴┴  └─┘┴└─");
+  console.log("v" + pack.version);
+  console.log("press ctrl+c to exit");
+  if (!without) {
+    progressBar = term.progressBar({
+      width: 80,
+      title: "progress: ",
+      eta: false,
+      percent: true,
+    });
+    console.log();
+    term.spinner({
+      animation: "asciiSpinner",
+    });
+  } else {
+    await file.readdir("./face", async (e, f) => {
+      if (e) {
+        term.red("run setup");
+        process.exit(0);
+      }
+      await file.readdir("./notface", (e, ff) => {
+        if (e) {
+          term.red("run setup");
+          process.exit(0);
+        }
+        counter = f.length + ff.length;
+        console.log("estimate of pre-progress: ", counter);
+      });
+    });
+  }
+}
+
+intro(true);
+
+/**
+ * safety now
+ */
+
+const r = require("readline");
+r.emitKeypressEvents(process.stdin);
+if (process.stdin.isTTY) {
+  process.stdin.setRawMode(true);
+}
+process.stdin.on("keypress", (s, k) => {
+  if (k.ctrl == true && k.name == "c") {
+    term.red("\nctrl+c pressed, now exitting\n");
+    process.exit(0);
+  }
 });
